7fc00e94123459df07bc85b1e1eb3cb82e153d2d
tomas <tomascostapp@gmail.com>
2025-04-06 17:42:01 -0300

    Cosas

--- pages/cursos/index.php
+++ pages/cursos/index.php
@@ -103,12 +103,13 @@ $totalPaginas = ceil($totalCursos / $porPagina);
 			<!-- Contenido principal -->
 			<div>
 				<div class="grid grid-cols-3 gap-4">
-					<?php foreach ($cursos as $curso): ?>
+					<?php foreach ($cursos as $curso): 
+						$es_premium = $curso['tipo'] == 'premium'?>
 						<a 	
-							href="/?page=detalles_cursos&id=<?php echo $curso['id']; ?>" 
+							href="<?php echo !$es_premium ? '/?page=detalles_cursos&id=' . $curso['id'] : '/#planpremium' ?>" 
 							class="p-4 rounded-sm border border-gray-300 relative"
 						>
-							<?php if ($curso['tipo'] == 'premium') : ?>
+							<?php if ($es_premium) : ?>
 								<span class="absolute top-2 right-2 bg-purple-700 text-white text-xs font-semibold px-2 py-2 rounded-full">
 									<svg class="size-[15px] fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
 								</span>
