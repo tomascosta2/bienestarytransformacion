3fb3c4b6dff6c745e4e1fd34d3da73c63fce2907
tomas <tomascostapp@gmail.com>
2025-04-05 21:37:57 -0300

    Boton Inscribirse a Registro

--- pages/registro-exitoso/index.php
+++ pages/registro-exitoso/index.php
@@ -2,12 +2,11 @@
 include("../../include/head.php");
 ?>
 
-<div class="container mx-auto py-[100px] px-4">
-	<div class="bg-white shadow-md rounded-lg p-6">
+<div class="container mx-auto py-[120px] px-4">
+	<div class="bg-white rounded-lg p-6">
 		<h1 class="text-4xl font-bold text-center mb-4">Registro Exitoso</h1>
 		<p class="text-gray-700 text-center mb-4">¡Tu registro ha sido exitoso! Solo falta un paso.</p>
 		<p class="text-gray-700 text-center mb-4">Porfavor confirma tu correo electronico en el email que te llego.</p>
-		<a href="../index.php" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Volver al inicio</a>
 	</div>
 </div>
 
