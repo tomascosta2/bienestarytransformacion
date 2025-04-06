64025630505460341aa14d05198dd16bcc40e9d8
tomas <tomascostapp@gmail.com>
2025-04-06 00:14:04 -0300

    Boton Inscribirse a Registro

--- controllers/procesar_ingreso.php
+++ controllers/procesar_ingreso.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             }
 
             $action = isset($_GET['action']) ? $_GET['action'] : '';
-            echo $action;
+            echo '<script>alert("' . $action . '");</script>';
             if ($action == 'pay') {
                 header("Location: /?page=plan_premium");
             } else {
